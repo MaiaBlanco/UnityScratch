9b57d453fedad84373e80ca12dfbd814fad28bfe
Mark Blanco <mrkblnc1248@gmail.com>
2016-10-02 22:40:31 -0400

    Adds "Roll a Ball" tutorial (in progress).

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject playerReference;
+    private Vector3 offsetValue;
+	// Use this for initialization
+	void Start () {
+        offsetValue = transform.position - playerReference.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = playerReference.transform.position + offsetValue;
+	}
+}
---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+    void Start()
+    {
+        speed = 10.0F;
+        rb = GetComponent<Rigidbody>();
+    }
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHoizontal = Input.GetAxis("Horizontal");
+        float moveVertical= Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHoizontal, 0.0f, moveVertical);
+        rb.AddForce(movement*speed);
+    }
+}
